98ad223a721dcb6e7fb59990a47eba9fb80e533a
Роман Ульянов <lntyrd@gmail.com>
2016-11-16 01:11:03 +0300

    initial commit

---
+++ SumPairsPrinter.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApplication1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class SumPairsPrinter
+    {
+        public static void PrintPairs(IEnumerable<int> col, int sum)
+        {
+            var arr = col.ToArray();
+            Array.Sort(arr);
+
+            int i = 0;
+            int j = arr.Length - 1;
+
+            while (i <= j)
+            {
+                var s = arr[i] + arr[j];
+
+                if (s == sum)
+                {
+                    Console.WriteLine($"{sum} = {arr[i]} + {arr[j]}");
+                    ++i;
+                    --j;
+                }
+                else
+                {
+                    if (s < sum)
+                        ++i;
+                    else
+                        --j;
+                }
+            }
+        }
+    }
+}
---
+++ SyncQueue.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApplication1
+{
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.Threading;
+
+    public class SyncQueue<T>
+    {
+        private Queue<T> internalQueue = new Queue<T>();
+        private EventWaitHandle haveItemsEvent = new ManualResetEvent(false);
+        private object popLock = new object();
+
+        public void Push(T item)
+        {
+            lock (((ICollection)internalQueue).SyncRoot)
+            {
+                internalQueue.Enqueue(item);
+
+                // Устанавливаем сигнал о том, что добавлен элемент
+                haveItemsEvent.Set();
+            }
+        }
+
+        public T Pop()
+        {
+            T poped;
+
+            // Нам необходимо, чтобы лишь один поток единовременно ожидал сигнала о том, что элемент добавлен
+            lock (popLock)
+            {
+                // Ждём сигнал
+                haveItemsEvent.WaitOne();
+
+                // Здесь заберем блокировку SyncRoot для синхронизации чтения\записи
+                lock (((ICollection)internalQueue).SyncRoot)
+                {
+                    poped = internalQueue.Dequeue();
+                    if (internalQueue.Count == 0)
+                    {
+                        haveItemsEvent.Reset();
+                    }
+                }
+            }
+            
+            return poped;           
+        }
+    }
+}
